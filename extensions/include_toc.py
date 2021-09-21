3becbe93fdfe58404efefea58b3cf73cc90c53e0
Ruth Fuchss <ruth.fuchss@canonical.com>
2021-09-21 16:49:00 +0200

    extension: put ToC on the side
    Add an extension that adds required HTML code to all markdown
    pages that include a "[TOC]" tag. The HTML code puts the table
    of contents on the side, or on the bottom with a link from the
    top in mobile view.
    
    Signed-off-by: Ruth Fuchss <ruth.fuchss@canonical.com>

---
+++ extensions/include_toc.py
@@ -0,0 +1,42 @@
+### Extension to include the HTML code needed to put the
+### Table of Contents into a sidebar.
+### If a markdown page contains "[TOC]", all required code
+### will be inserted.
+
+from markdown.preprocessors import Preprocessor
+from markdown.extensions import Extension
+
+class InsertToCStuff(Preprocessor):
+
+    def run(self, lines):
+
+        if '[TOC]' in str(lines):
+            # If there is a [TOC] tag in the markdown file, add the following
+            # lines to the top of the file.
+
+            new_lines = ['<div class="col-9 u-wrap-content" markdown="1">\n',
+                         '<div class="toc-link u-hide--medium u-hide--large">\n',
+                         '<a href="#table-of-contents">Go to Table of Contents</a>\n',
+                         '</div>\n']
+        else:
+            # If there is no [TOC] tag, leave the file unchanged.
+            return lines
+
+        # Keep all lines that do not contain "[TOC]".
+        for line in lines:
+            if not '[TOC]' in line:
+                new_lines.append(line)
+
+        # Add the following lines to the bottom of the file.
+        new_lines.extend(['</div><div class="col-3 u-wrap-content" markdown="block">\n',
+                          '<a name="table-of-contents"></a>',
+                          '\n',
+                          '[TOC]\n'])
+
+        return new_lines
+
+
+class IncludeToCExtension (Extension):
+
+    def extendMarkdown(self, md, md_globals):
+        md.preprocessors.add('includetoc', InsertToCStuff(md), '_begin')
